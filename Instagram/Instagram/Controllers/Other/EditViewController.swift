a8b8af9d3e944f3c5fd0b75e49bbf6a3586d582e
Bertrand Shao <shaob@g.ucla.edu>
2020-08-29 17:25:36 -0700

    Built out nav bar

---
+++ Instagram/Instagram/Controllers/Other/EditViewController.swift
@@ -0,0 +1,22 @@
+//
+//  EditViewController.swift
+//  Instagram
+//
+//  Created by Bertrand Shao on 8/29/20.
+//  Copyright © 2020 Bertrand Shao. All rights reserved.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
---
+++ Instagram/Instagram/Controllers/Other/PostViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PostViewController.swift
+//  Instagram
+//
+//  Created by Bertrand Shao on 8/29/20.
+//  Copyright © 2020 Bertrand Shao. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
