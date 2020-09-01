10bdf5f3cd0aa72a93e1952d0b18473eea495c70
Bertrand Shao <shaob@g.ucla.edu>
2020-08-31 19:39:00 -0700

    Add podfile

---
+++ Instagram/Instagram/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Bertrand Shao on 8/28/20.
+//  Copyright © 2020 Bertrand Shao. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
